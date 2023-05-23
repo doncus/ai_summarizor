5f868013dd4b9ae1323f547c9d8d9bd9efe41958
Donc <dominic.handl@hotmail.com>
2023-05-23 21:41:12 +0200

    finish summarizor

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,22 +3,50 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 
+let timeout;
+
 const Demo = () => {
   const [article, setArticle] = useState({
-    url: "",
+    url: "https://time.com/6266679/musk-ai-open-letter/",
     summary: "",
   });
+  const [allArticles, setAllArticles] = useState([]);
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  const [copied, setCopied] = useState("");
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+    if (articlesFromLocalStorage) setAllArticles(articlesFromLocalStorage);
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const { data } = await getSummary({ articleUrl: article.url });
+    console.log({ articleUrl: article.url });
+
     if (data?.summary) {
+      console.log("there");
+      console.log(data.summary);
       const sumArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [sumArticle, ...allArticles];
+
       setArticle(sumArticle);
+      setAllArticles(updatedAllArticles);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
       console.log(sumArticle);
     }
+  };
+
+  const handleCopy = (copyUrl) => {
+    clearTimeout(timeout);
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+
+    timeout = setTimeout(() => setCopied(false), 3000);
   };
 
   return (
@@ -49,8 +77,54 @@ const Demo = () => {
           </button>
         </form>
         {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                <img
+                  src={copied === item.url ? tick : copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
       {/* Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, that wasn't supposed to happen...
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
--- src/services/article.js
+++ src/services/article.js
@@ -1,27 +1,30 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const rapidAPIKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
     prepareHeaders: (headers) => {
-      headers.set("X-RapidAPI-Key", rapidAPIKey);
+      headers.set("X-RapidAPI-Key", rapidApiKey);
       headers.set(
         "X-RapidAPI-Host",
         "article-extractor-and-summarizer.p.rapidapi.com"
       );
-
       return headers;
     },
   }),
   endpoints: (builder) => ({
     getSummary: builder.query({
-      query: (params) => `/summarize?url=${params.articleUrl}&length=3`,
+      query: (params) =>
+        `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
     }),
   }),
 });
 
+// when you want to call it immediately after the render
 // export const { useGetSummaryQuery } = articleApi;
+
+// when you want to call it on-demand (submit button clicked)
 export const { useLazyGetSummaryQuery } = articleApi;
